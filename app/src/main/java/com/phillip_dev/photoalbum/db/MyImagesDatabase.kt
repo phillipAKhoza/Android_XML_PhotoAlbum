513909cebd6dad667fb8362f084a5ad4da0a76f3
phillip khoza <phillipantonk@gmail.com>
2024-03-22 11:08:59 +0200

    add database class [annotate class as db]

--- app/src/main/java/com/phillip_dev/photoalbum/db/MyImagesDatabase.kt
+++ app/src/main/java/com/phillip_dev/photoalbum/db/MyImagesDatabase.kt
@@ -1,4 +1,9 @@
 package com.phillip_dev.photoalbum.db
 
+import androidx.room.Database
+import androidx.room.Entity
+import com.phillip_dev.photoalbum.model.MyImages
+
+@Database(entities = [MyImages::class], version = 1)
 class MyImagesDatabase {
 }
