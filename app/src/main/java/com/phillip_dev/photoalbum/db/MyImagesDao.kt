c93edec9365d1176bbd56905b8fd3771e501e908
phillip khoza <phillipantonk@gmail.com>
2024-03-22 09:53:38 +0200

    add image dao interface [add insert fun]

--- app/src/main/java/com/phillip_dev/photoalbum/db/MyImagesDao.kt
+++ app/src/main/java/com/phillip_dev/photoalbum/db/MyImagesDao.kt
@@ -1,7 +1,12 @@
 package com.phillip_dev.photoalbum.db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import com.phillip_dev.photoalbum.Model.MyImages
 
 @Dao
 interface MyImagesDao {
+
+    @Insert
+    suspend fun addImage(myImages: MyImages)
 }
