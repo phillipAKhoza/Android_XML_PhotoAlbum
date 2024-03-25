953020644182524c49e6b28c7344e5ce4c345da2
phillip khoza <phillipantonk@gmail.com>
2024-03-25 22:44:42 +0200

    add view model in main

--- app/src/main/java/com/phillip_dev/photoalbum/view/MainActivity.kt
+++ app/src/main/java/com/phillip_dev/photoalbum/view/MainActivity.kt
@@ -2,11 +2,17 @@ package com.phillip_dev.photoalbum.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.phillip_dev.photoalbum.R
+import com.phillip_dev.photoalbum.viewmodel.MyImagesViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myImagesViewModel: MyImagesViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myImagesViewModel = ViewModelProvider(this).get(MyImagesViewModel::class.java)
     }
 }
