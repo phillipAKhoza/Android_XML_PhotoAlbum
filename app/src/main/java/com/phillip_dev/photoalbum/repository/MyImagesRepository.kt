5db1a4f9370c0f73d261749758f22419a3b62ce6
phillip khoza <phillipantonk@gmail.com>
2024-03-25 22:01:18 +0200

    add repo class [add init block[add db ]]

--- app/src/main/java/com/phillip_dev/photoalbum/repository/MyImagesRepository.kt
+++ app/src/main/java/com/phillip_dev/photoalbum/repository/MyImagesRepository.kt
@@ -1,15 +1,19 @@
 package com.phillip_dev.photoalbum.repository
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import com.phillip_dev.photoalbum.db.MyImagesDao
+import com.phillip_dev.photoalbum.db.MyImagesDatabase
 import com.phillip_dev.photoalbum.model.MyImages
 
-class MyImagesRepository {
+
+class MyImagesRepository(application: Application) {
 
     var myImagesDao : MyImagesDao
     var imageList : LiveData<List<MyImages>>
 
     init {
+        val db = MyImagesDatabase.getDatabaseInstance(application)
 
     }
 }
