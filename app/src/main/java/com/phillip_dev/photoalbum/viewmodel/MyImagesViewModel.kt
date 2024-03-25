8d5c0675af5512654524c651dc4cf7c55200019f
phillip khoza <phillipantonk@gmail.com>
2024-03-25 22:31:42 +0200

    add view model class [add repo var]

--- app/src/main/java/com/phillip_dev/photoalbum/viewmodel/MyImagesViewModel.kt
+++ app/src/main/java/com/phillip_dev/photoalbum/viewmodel/MyImagesViewModel.kt
@@ -2,6 +2,12 @@ package com.phillip_dev.photoalbum.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.phillip_dev.photoalbum.model.MyImages
+import com.phillip_dev.photoalbum.repository.MyImagesRepository
 
 class MyImagesViewModel(application: Application) : AndroidViewModel(application) {
+
+    var repository : MyImagesRepository
+
 }
