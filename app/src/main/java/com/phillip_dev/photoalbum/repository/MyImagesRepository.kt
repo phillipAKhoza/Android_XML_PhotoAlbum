555950a93bad49dc9375911c9ba85ada3726967a
phillip khoza <phillipantonk@gmail.com>
2024-03-25 21:58:40 +0200

    add repo class [add image array]

--- app/src/main/java/com/phillip_dev/photoalbum/repository/MyImagesRepository.kt
+++ app/src/main/java/com/phillip_dev/photoalbum/repository/MyImagesRepository.kt
@@ -1,8 +1,11 @@
 package com.phillip_dev.photoalbum.repository
 
+import androidx.lifecycle.LiveData
 import com.phillip_dev.photoalbum.db.MyImagesDao
+import com.phillip_dev.photoalbum.model.MyImages
 
 class MyImagesRepository {
 
     var myImagesDao : MyImagesDao
+    var imageList : LiveData<List<MyImages>>
 }
