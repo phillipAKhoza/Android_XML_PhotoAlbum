63562d2fde2a4e8ea00f3c9d8f6d6d446d8e5df7
phillip khoza <phillipantonk@gmail.com>
2024-03-28 23:25:11 +0200

    add add image view

---
+++ app/src/main/java/com/phillip_dev/photoalbum/view/AddImageActivity.kt
@@ -0,0 +1,12 @@
+package com.phillip_dev.photoalbum.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.phillip_dev.photoalbum.R
+
+class AddImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_image)
+    }
+}
